cb87f4cd61b05909cc0a5d2afab8281034a61013
Олег <OllegOzz@yandex.ru>
2024-02-08 20:25:25 +0300

    slide swiper

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,29 @@
-import mobileNav from './modules/mobile-nav.js';
-mobileNav();
+// import mobileNav from './modules/mobile-nav.js';
+// mobileNav();
+
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+import 'swiper/css/bundle';
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    // direction: 'vertical',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
